595a7aee6ac2d48732e4a59a21a14c8f29890c36
pawel.klapuch <pawel.klapuch@spot.pl>
2017-05-26 03:57:14 +0200

    project structure changed

---
+++ Shared/Core.h
@@ -0,0 +1,20 @@
+//
+// Created by Pawel Klapuch on 14/05/2017.
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+    
+#ifndef _Core_
+#define _Core_
+
+const char* version();
+    
+#endif
+
+#ifdef __cplusplus
+}
+#endif
